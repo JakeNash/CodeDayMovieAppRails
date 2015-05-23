901f35df6303a42cb78fe9182fa631f7c5acac8b
Jake Nash <nashj2@uw.edu>
2015-05-23 16:58:07 -0700

    Initial commit with stuff from other repo

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,80 @@
+# This is the apiv2base controller. Yay
+class BaseController < ApplicationController
+  include Devise::Controllers::Helpers
+
+  respond_to :json
+
+  # helper_method :url_for_next_page
+  # helper_method :url_for_previous_page
+  helper_method :extend_params
+  helper_method :json_error
+
+  before_filter :log_user_agent
+  before_filter :authenticate_user!
+
+  after_filter :cors_set_access_control_headers
+  skip_before_filter :authenticate_user!, :only => [:cors_preflight_check]
+
+  # before_action :set_x_organization_header
+
+  has_scope :page, default: 1
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Token'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+  private
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+
+  def log_user_agent
+    logger.info "USERAGENT: #{request.headers['HTTP_USER_AGENT']}"
+  end
+
+  def extend_params(params)
+    request.query_parameters.merge(params)
+  end
+
+  # def url_for_next_page(collection, options = {})
+  #   return nil if !collection.limit_value || collection.last_page?
+  #   url_api_page(collection.current_page + 1)
+  # end
+
+  # def url_for_previous_page(collection, options = {})
+  #   return nil if !collection.limit_value || collection.first_page?
+  #   url_api_page(collection.current_page - 1)
+  # end
+
+  # def url_api_page(page)
+  #   url = request.protocol + request.host_with_port + request.path
+  #   url += "?#{extend_params(Kaminari.config.param_name => page).to_param}"
+  # end
+  
+  # def set_x_organization_header
+  #   response.headers["X-Organization-Last-Modified"] = current_organization.cached_at.try(:httpdate) if current_organization
+  # end
+
+  def json_error(obj)
+    return json_error_hash(obj) if obj.is_a?(Hash)
+    {errors: obj.errors.to_hash.merge({_full_messages: obj.errors.full_messages, _full_message: obj.errors.full_messages.join(', ')})}
+  end
+
+  def json_error_hash(hash)
+    msg_array = hash.map { |key,value| "#{key.capitalize} #{value}"}
+    {errors: hash.merge( _full_messages: msg_array, _full_message: msg_array.join(', '))}
+  end
+
+end
---
+++ app/controllers/token_controller.rb
@@ -0,0 +1,44 @@
+#This controller allows you to get a users api token with a given email/password combo
+class TokenController < BaseController
+
+  skip_before_filter :authenticate_user!
+  # skip_before_filter :set_raven_context
+  respond_to :json
+  action :only => [:create]
+
+  def create
+    @user = User.new
+
+    if !params_present?
+      @user.errors.add(:email, 'not present') if !params[:user][:email]
+      @user.errors.add(:password, 'not present') if !params[:user][:password]
+    end
+
+    if !@user.errors.any?
+      user_to_authenticate = User.find_for_database_authentication(:email => params[:user][:email])
+      @user.errors.add(:email, 'address not found') if user_to_authenticate.nil?
+    end
+
+
+    if !@user.errors.any?
+      @user = user_to_authenticate
+
+      @user.errors.add(:password, 'could not be authenticated successfully') if !user_to_authenticate.valid_password?(params[:user][:password])
+    end
+
+    if @user.errors.any?
+      render :show,
+             :status => 422
+    else
+      render :show
+    end
+  end
+  
+  private
+
+  def params_present?
+    user = params[:user]
+    user and user.is_a?(HashWithIndifferentAccess) and user[:email] and user[:password]
+  end
+
+end
