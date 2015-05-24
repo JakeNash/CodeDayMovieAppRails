72ab908757d06d4524d8eb932432ac35d4ac0801
Jake Nash <nashj2@uw.edu>
2015-05-24 04:30:57 -0700

    Made a small change

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -35,6 +35,7 @@ class BaseController < ApplicationController
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Max-Age'] = "1728000"
   end
